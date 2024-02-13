1b60e87071fa004fc8efc069c7884ae14ead6d9a
?0 <benjamindguntara@protonmail.com>
2024-02-13 13:47:20 +0700

    add history page

--- src/urls.py
+++ src/urls.py
@@ -6,14 +6,15 @@ from django.conf import settings
 
 urlpatterns = [
         path("",views.index, name="index"),
-        path("register/",views.register, name="register"),
-        path("user/home/",views.home, name="home"),
-        path("user/profile/",views.profile, name="profile"),
-        path("user/order/",views.order,name="order"),
         path("accounts/",include("django.contrib.auth.urls")),
+        path("accounts/signup",views.register, name="register"),
         path("accounts/logout/",auth_views.LogoutView.as_view,name="logout"),
         path("accounts/password_change/",auth_views.PasswordChangeView.as_view,name="password_change"),
         path("accounts/password_change/done/",auth_views.PasswordChangeDoneView.as_view,name="password_change_done"),
         path("accounts/password_reset/",auth_views.PasswordResetView.as_view,name="password_reset"),
-        path("accounts/password_reset/done/",auth_views.PasswordResetDoneView.as_view,name='password_reset_done')
+        path("accounts/password_reset/done/",auth_views.PasswordResetDoneView.as_view,name='password_reset_done'),
+        path("user/home/",views.home, name="home"),
+        path("user/profile/",views.profile, name="profile"),
+        path("user/order/",views.order,name="order"),
+        path("user/history/",views.history,name="history")
 ]
