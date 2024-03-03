cde4cc6bd32bc4946f67074d2d0973a91f0720ef
?0 <benjamindguntara@protonmail.com>
2024-03-02 21:34:15 -0800

    add honeypot

--- WebInventory/settings.py
+++ WebInventory/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
+    "admin_honeypot",
     'jazzmin',
     'django.contrib.admin',
     'django.contrib.auth',
--- src/admin.py
+++ src/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Item, Order
+from .models import *
 
 # Register your models here.
 
@@ -7,12 +7,17 @@ admin.site.site_header = "Administrator Panel"
 admin.site.site_title = "Web Inventory"
 admin.site.index_title = "Dashboard"
 
-class ItemAdmin(admin.ModelAdmin):
+class ItemConfigAdmin(admin.ModelAdmin):
     list_display = ("name","category","condition","quantity","room")
     list_filter = ["category","room","condition"]
     
-class OrderAdmin(admin.ModelAdmin):
+class OrderConfigAdmin(admin.ModelAdmin):
     list_display = ("client","product","order_quantity","date")
 
-admin.site.register(Item,ItemAdmin)
-admin.site.register(Order, OrderAdmin)
+class SiswaConfigAdmin(admin.ModelAdmin):
+    list_display = ("username","jurusan","kelas","nomortelp","email","date_joined","last_login")
+    list_filter = ["jurusan","kelas"]
+
+admin.site.register(tbl_barang,ItemConfigAdmin)
+admin.site.register(tbl_order, OrderConfigAdmin)
+admin.site.register(tbl_siswa,SiswaConfigAdmin)
